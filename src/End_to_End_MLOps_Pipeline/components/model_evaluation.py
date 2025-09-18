4e9abab722c51014cdc22a7633378c92fc3c31cf
Ashwin Balaji <118180281+AshwinBalaji007@users.noreply.github.com>
2025-09-18 16:21:21 +0000

    Flask app added

--- src/End_to_End_MLOps_Pipeline/components/model_evaluation.py
+++ src/End_to_End_MLOps_Pipeline/components/model_evaluation.py
@@ -15,54 +15,57 @@ class ModelEvaluation:
     def __init__(self, config: ModelEvaluationConfig):
         self.config = config
 
-    
-    def eval_metrics(self,actual, pred):
+    def eval_metrics(self, actual, pred):
         rmse = np.sqrt(mean_squared_error(actual, pred))
         mae = mean_absolute_error(actual, pred)
         r2 = r2_score(actual, pred)
         return rmse, mae, r2
-    
-
 
     def log_into_mlflow(self):
-
         test_data = pd.read_csv(self.config.test_data_path)
         model = joblib.load(self.config.model_path)
 
         test_x = test_data.drop([self.config.target_column], axis=1)
         test_y = test_data[[self.config.target_column]]
 
-
         mlflow.set_registry_uri(self.config.mlflow_uri)
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
 
-
         with mlflow.start_run():
-
             predicted_qualities = model.predict(test_x)
-
             (rmse, mae, r2) = self.eval_metrics(test_y, predicted_qualities)
-            
-            # Saving metrics as local
+
+            # Save metrics locally
             scores = {"rmse": rmse, "mae": mae, "r2": r2}
             save_json(path=Path(self.config.metric_file_name), data=scores)
 
+            # Log hyperparameters
             mlflow.log_params(self.config.all_params)
 
+            # Log metrics
             mlflow.log_metric("rmse", rmse)
             mlflow.log_metric("r2", r2)
             mlflow.log_metric("mae", mae)
 
+            # Log metrics.json as artifact
+            mlflow.log_artifact(self.config.metric_file_name, artifact_path="metrics")
 
-            # Model registry does not work with file store
+            # Prepare example for MLflow signature
+            input_example = test_x.iloc[:5]
+
+            # Register the model
             if tracking_url_type_store != "file":
-
-                # Register the model
-                # There are other ways to use the Model Registry, which depends on the use case,
-                # please refer to the doc for more information:
-                # https://mlflow.org/docs/latest/model-registry.html#api-workflow
-                mlflow.sklearn.log_model(model, "model", registered_model_name="ElasticnetModel")
+                mlflow.sklearn.log_model(
+                    model,
+                    "model",
+                    registered_model_name="ElasticnetModel",
+                    input_example=input_example,
+                )
             else:
-                mlflow.sklearn.log_model(model, "model")
+                mlflow.sklearn.log_model(
+                    model,
+                    "model",
+                    input_example=input_example,
+                )
 
     
